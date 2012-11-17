dc3fb3d24f8eafc6de4f6480768cd1a110549200
Jacob Rieger <jacobriegerg>
2012-11-16 18:01:43 -0700

    Added "paint" feature to click. Tested more cube movement

--- PhysBlock/Assets/Scripts/Cube.cs
+++ PhysBlock/Assets/Scripts/Cube.cs
@@ -4,13 +4,12 @@ using System.Collections;
 public class Cube : MonoBehaviour {
 	
 	private bool colliding = false;
-	public string type1;
-	private string type2;
-	private string type3;
+	public string weight;
+	
 	// Use this for initialization
 	void Start () {
 	
-		type1 = "normal";
+		
 	}
 	
 	// Update is called once per frame
@@ -18,6 +17,11 @@ public class Cube : MonoBehaviour {
 		
 		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 		transform.localEulerAngles = new Vector3(0,0,0);	
+	}
+	
+	void changeMagnetic()
+	{
+		
 	}
 
 }
