50bb29736c45993dec6bca8b7e859fdc13bac55e
Don England <englandpro@gmail.com>
2012-11-18 23:18:55 -0700

    Add Basic Materials and Prefabs

---
+++ PhysBlock/Assets/Scripts/temp.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class temp : MonoBehaviour {
+	
+	private GameObject myChild;
+
+	// Use this for initialization
+	void Start () {
+		myChild = Instantiate(Resources.Load("IceCube", typeof(GameObject))) as GameObject;
+		myChild.transform.parent = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Destroy(myChild);
+			myChild = Instantiate (Resources.Load("GreenCube", typeof(GameObject))) as GameObject;
+			myChild.transform.position += new Vector3 (1,1,1);
+			myChild.transform.parent = gameObject.transform;
+		}
+	}
+}
