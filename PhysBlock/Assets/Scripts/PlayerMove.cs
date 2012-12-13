53df19a31285d6ef4b21430e3938de98f97c5293
Jeff Hyatt <jahyatt8021@gmail.com>
2012-12-12 18:30:51 -0700

    Added ability to squat.  Animation is fairly low-key, so might not be noticeable.

--- PhysBlock/Assets/Scripts/PlayerMove.cs
+++ PhysBlock/Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
-
+	
+	private Animator animator;
+	
+	void Start(){
+		animator = gameObject.GetComponent<Animator>() as Animator;
+	}
 	
 	void Update () {
 		
@@ -11,6 +16,7 @@ public class PlayerMove : MonoBehaviour {
 			Debug.Log ("Trying to call MoveTo");
 			RaycastHit hit;
 			Vector3 NewPosition = Vector3.zero;
+			animator.SetBool("Squat",true);
 			if (Physics.Raycast (transform.position, Vector3.right, out hit, 1f)) 
 			{
 				NewPosition = new Vector3(hit.collider.transform.position.x + 1f,
@@ -33,7 +39,8 @@ public class PlayerMove : MonoBehaviour {
 				Debug.Log ("No Raycast hit");
 			}
 			
-			
+		} else {
+			animator.SetBool("Squat",false);
 		}
 	
 	}
